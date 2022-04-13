75c4139d177c1b7d42681ba63ac2fbae0a83f02b
Jana Pascual <jacpascual@gmail.com>
2022-04-13 12:52:58 -0400

    Adds index functionality and index show html

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,13 +1,15 @@
 # core/views.py 
 
 from flask import render_template, request, Blueprint
+from myapp.models import SuccessStory
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    success_stories = SuccessStory.query.order_by(SuccessStory.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', success_stories=success_stories)
 
 @core.route('/info')
 def info():
